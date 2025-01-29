4e56026653648a98b2caef32caef9743384a7f41
sofiage <sofiageoldes@yahoo>
2025-01-29 22:50:26 +0200

    Mid Term Quiz Drawing Tool

---
+++ src/main/java/org/drawing/DrawingMain.java
@@ -0,0 +1,22 @@
+package org.drawing;
+
+public class DrawingMain {
+
+    public static void main( String[] args )
+    {
+        DrawingTool drawingTool = new DrawingTool();
+
+        drawingTool.addShape(new Rectangle(5, 15));
+        drawingTool.addShape(new Square(4));
+        drawingTool.addShape((new Circle(3.8)));
+        drawingTool.addShape(new Triangle(4, 5, 10, 11, 12));
+
+        drawingTool.addShape(new Rectangle(10, 6)); //This is a duplicate and will print a message
+
+
+        drawingTool.displayShapes();
+        System.out.println("The total amount of the surface is " + drawingTool.calculateTotalOfSurface());
+        System.out.println("The total amount of the perimeter is " + drawingTool.calculateTotalOfPerimeter());
+
+    }
+}
---
+++ src/main/java/org/drawing/Shapable.java
@@ -0,0 +1,8 @@
+package org.drawing;
+
+public interface Shapable {
+
+    double calculateSurface();
+    double calculatePerimeter();
+
+}
