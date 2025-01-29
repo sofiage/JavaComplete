be73822a4210fffaafbd67e868c52547cba14407
sofiage <sofiageoldes@yahoo.com>
2025-01-30 00:45:48 +0200

    Exceptions and Loggings Homework

---
+++ src/main/java/org/student/Student.java
@@ -0,0 +1,74 @@
+package org.student;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Student implements Comparable<Student> {
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String gender;
+    private String id;
+
+
+    public Student(String firstName, String lastName, LocalDate dateOfBirth, String gender, String id) {
+        if (firstName == null || firstName.isEmpty()) {
+            throw new IllegalArgumentException("First name cannot be empty");
+        }
+        if (lastName == null || lastName.isEmpty()) {
+            throw new IllegalArgumentException("Last name cannot be empty");
+        }
+        if (dateOfBirth == null || dateOfBirth.getYear() < 1900 || dateOfBirth.isAfter(LocalDate.now().minusYears(18))) {
+            throw new IllegalArgumentException("Date of birth must be between 1900 and current date and the student must be over 18");
+        }
+        if (!(gender.equalsIgnoreCase("male") || gender.equalsIgnoreCase("female") || gender.equalsIgnoreCase("M") || gender.equalsIgnoreCase("F"))) {
+            throw new IllegalArgumentException("Gender must be male/female or M/F");
+        }
+        if (id == null || id.isEmpty()) {
+            throw new IllegalArgumentException("ID cannot be empty");
+        }
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.id = id;
+    }
+
+    public int calculateAge() {
+        return Period.between(dateOfBirth, LocalDate.now()).getYears();
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public int compareTo(Student other) {
+        int lastNameComparison = this.lastName.compareTo(other.lastName);
+        if (lastNameComparison != 0) {
+            return lastNameComparison;
+        } else {
+            return this.dateOfBirth.compareTo(other.dateOfBirth);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "StudentRepository{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", gender='" + gender + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+}
