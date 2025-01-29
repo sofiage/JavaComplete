33afbd1e98cc9a76d4588a399b82ea45d4a13c47
Sofia Geoldes <sofiageoldes@yahoo.com>
2025-01-29 18:19:17 +0200

    i tema library

---
+++ src/main/java/org/library/LibraryCatalog.java
@@ -0,0 +1,43 @@
+package org.library;
+import java.util.ArrayList;
+
+public class LibraryCatalog {
+    ArrayList<Book> catalog = new ArrayList<>();
+
+    public void addBook(Book book) {
+        if (!catalog.contains(book)) {
+            catalog.add(book);
+            System.out.println("The book " + book + " was added");
+        } else {
+            System.out.println("\nThe book already exists");
+        }
+    }
+
+    public void removeBook(Book book) {
+        if (catalog.remove(book)) {
+            System.out.println("\nBook " + book.name + " was removed ");
+        } else {
+            System.out.println("\nBook not found");
+        }
+    }
+
+    public void listBooks() {
+        for (Book book : catalog) {
+            System.out.println("\n Book: " + book.name + ", Pages: " + book.numberOfPages);
+            if (book instanceof Novel) {
+                System.out.println("\nType: " + ((Novel) book).getType());
+            } else if (book instanceof ArtAlbum) {
+                System.out.println("\nPaper quality: " + ((ArtAlbum) book).getPaperQuality());
+
+
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "LibraryCatalog{" +
+                "catalog=" + catalog +
+                '}';
+    }
+}
---
+++ src/main/java/org/library/LibraryManage.java
@@ -0,0 +1,7 @@
+package org.library;
+
+public interface LibraryManage {
+    void addBook(Book book);
+    void deleteBook(int index);
+    void listBooks();
+}
